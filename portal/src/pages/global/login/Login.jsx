2de4a3f1af638066d44b63109f37c868a91923ed
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-05-29 21:40:33 +0530

    login page added

--- portal/src/pages/global/login/Login.jsx
+++ portal/src/pages/global/login/Login.jsx
@@ -3,7 +3,9 @@ import classes from './login.module.css'
 const Login = ()=>{
     return(
         <div className={classes.ctn}>
-            
+            <input type="text" placeholder='Enter username' className={classes.ip_f} />
+            <input type="password" placeholder='Enter password' className={classes.ip_f}/>
+            <button className={classes.btn}>Log In</button>
         </div>
     )
 }
